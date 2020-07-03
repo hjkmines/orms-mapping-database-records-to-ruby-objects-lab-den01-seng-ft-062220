51df221fd4ca1e265311790d126a1a5bf9a5554a
Hyung Kim <hjkmines@gmail.com>
2020-07-03 04:41:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
   end 
   
   def self.first_X_students_in_grade_10(num_of_students)
-    sql <<-SQL 
+    sql = <<-SQL 
     SELECT * 
     FROM students
     WHERE students.grade = 10
