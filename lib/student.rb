b5cc72b667f877ab5bc207ebec5ed7e162e8b338
Hyung Kim <hjkmines@gmail.com>
2020-07-03 04:52:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,9 +78,9 @@ class Student
   end 
   
   def self.first_student_in_grade_10 
-    s = self.first_student_in_grade_10(1)
-    s.map do |row|
-      self.new_from_db(row)
+  s = self.first_X_students_in_grade_10(1)
+    s.map do |i|
+      self.new_from_db(i)
     end[0]
 
   end 
