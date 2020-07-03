67083417ceb1997eef7b2895f530678a9c506087
Hyung Kim <hjkmines@gmail.com>
2020-07-03 04:41:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,6 +67,17 @@ class Student
     end 
   end 
   
+  def self.first_X_students_in_grade_10(num_of_students)
+    sql <<-SQL 
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT num_of_students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
   
   def self.create_table
     sql = <<-SQL
